343bafb92a86a3c5475b9bf611dc8e77d96a6366
Franciszek Sowul <2482997s@student.gla.ac.uk>
2021-01-30 09:54:02 +0100

    category form + missing slash

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -18,6 +19,7 @@ def about(request):
     return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
+    print("success")
     context_dict = {}
 
     try:
@@ -31,4 +33,18 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    
+    return render(request, 'rango/add_category.html', {'form': form})
 
