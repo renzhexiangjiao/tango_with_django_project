72becb902f21595f96bda25091c51b2153f6979e
Franciszek Sowul <2482997s@student.gla.ac.uk>
2021-02-08 23:46:17 +0100

    chapter 10

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
@@ -15,10 +16,33 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = most_viewed_pages
 
+    visitor_cookie_handler(request)
+
     return render(request, 'rango/index.html', context=context_dict)
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by the Python God himself'}
+    context_dict = {'boldmessage': 'This tutorial has been put together by the Python God himself',
+                    'visits': request.session['visits']}
     return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
